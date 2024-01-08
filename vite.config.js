7ed4a8499c08bbc8d84bf4e7cdbfc057e52e7b13
md sajjad ansari <sajjadansari1384@gmail.com>
2024-01-08 01:45:40 -1000

    kk

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"https://github.com/sajjad1384N/Weather_App.git",
   plugins: [react()],
 })
