b08caf972d8d536f13f589a2bf4b1eef1dfe83fb
md sajjad ansari <sajjadansari1384@gmail.com>
2024-01-08 10:10:26 -1000

    fsf

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -9,9 +9,9 @@ export default function SearchBox({updateInfo}){
         marginLeft:"10px",marginTop:"15px",
     }
     let [city,setCity] = useState("");
-    let[error,setError]=useState("false");
+    // let[error,setError]=useState("false");
     let getWeatherApp=async()=>{
-        try {
+        
       let response=  await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
       let jsonResponse= await response.json();
      console.log(jsonResponse);
@@ -26,10 +26,7 @@ export default function SearchBox({updateInfo}){
       };
       console.log(result);
       return result;
-    } catch (err){
-        throw err;
-
-    }
+    
     };
     let handleChange = (evt)=>{
       setCity(evt.target.value);
@@ -57,7 +54,6 @@ export default function SearchBox({updateInfo}){
     variant="contained"style={styles} type="submit" >
         Search
       </Button>
-      { error && <p style={{color:"red"}}> No  such place exit</p> }
       </form>
     </div> 
     )
